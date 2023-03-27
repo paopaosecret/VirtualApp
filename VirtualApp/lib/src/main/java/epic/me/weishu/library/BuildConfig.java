965ad56125ee108bd7548416fe8138f6a934e25d
xanderzhao <xanderzhao@tencent.com>
2023-03-27 16:16:51 +0800

    不过滤QQ和微信

---
+++ VirtualApp/lib/src/main/java/epic/me/weishu/library/BuildConfig.java
@@ -0,0 +1,18 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package epic.me.weishu.library;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = false;
+    public static final String APPLICATION_ID = "epic.me.weishu.library";
+    public static final String BUILD_TYPE = "release";
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = 1;
+    public static final String VERSION_NAME = "1.0";
+
+    public BuildConfig() {
+    }
+}
